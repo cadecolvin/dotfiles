fcebc4f787888a011dbf9305d7dab0918a47dee9
Cade Colvin <cade.colvin@gmail.com>
2016-10-19 23:14:59 -0500

    Added zshrc link to the setup script

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@
 
 dotfiles=~/dotfiles
 dotfilesBackup=~/dotfiles_bak
-files="bashrc bash_profile gitconfig vimrc"
+files="bashrc bash_profile gitconfig vimrc zshrc"
 
 echo "Creating $dotfilesBackup directory to backup current dotfiles."
 mkdir -p $dotfilesBackup
