64f1361e06b911068eab1f07999fb1b5da8f8df4
Cade Colvin <cade.colvin@gmail.com>
2017-06-21 11:21:24 -0500

    Put oh-my-zsh install back at the top so it doesn't overwrite my zshrc

--- setup.sh
+++ setup.sh
@@ -5,26 +5,40 @@
 # then deploys dotfiles
 
 
+# Install OhMyZsh
+echo "Installing Oh-My-Zsh..." 
+curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -o install_omz.sh
+chmod 755 install_omz.sh
+exit | ( ./install_omz.sh > /dev/null )
+rm install_omz.sh
+echo "done!"
+
+# Create a link to my custom oh-my-zsh theme
+echo -n "Creating symlink to oh-my-zsh theme..."
+mkdir -p ~/.oh-my-zsh/custom/themes
+ln -fs $dotfiles/cadecolvin.zsh-theme ~/.oh-my-zsh/custom/themes/cadecolvin.zsh-theme
+echo "done!"
+
+
+# Symlink to all dotfiles in package
 dotfiles=$PWD
 backup=$dotfiles/bak
 files="bashrc bash_profile gitconfig xinitrc Xmodmap Xresources vimrc zshrc"
 
-# Symlink to all dotfiles in package
 echo -n "Creating $backup directory for current dotfiles."
 mkdir -p $backup
 echo "done"
 
 for file in $files; do
     if [ -f $file ]; then
-        echo -n "Moving $file to $backup"
+        echo -n "Moving $file to $backup..."
         mv ~/.$file $backup/
-        echo "done"
+        echo "done!"
     fi
 
-    echo -n "Creating symlink to $file"
+    echo -n "Creating symlink to $file..."
     ln -fs $dotfiles/$file ~/.$file
-    echo "done"
-    read temp
+    echo "done!"
 done
 
 # Setup i3 config files
@@ -36,23 +50,9 @@ ln -fs $dotfiles/i3statusconfig ~/.config/i3status/config
 # Check for and install Vundle and Vim plugins
 echo -n "Installing Vundle..."
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim > /dev/null
-echo "done"
+echo "done!"
 echo -n "Installing Vim Plugins..."
 vim +PluginInstall +qa
-echo "done"
-
-# Check for and install OhMyZsh
-echo "Installing Oh-My-Zsh..." 
-curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -o install_omz.sh
-chmod 755 install_omz.sh
-exit | ( ./install_omz.sh > /dev/null )
-rm install_omz.sh
-echo "done"
-
-# Create a link to my custom oh-my-zsh theme
-echo -n "Creating symlink to oh-my-zsh theme..."
-mkdir -p ~/.oh-my-zsh/custom/themes
-ln -fs $dotfiles/cadecolvin.zsh-theme ~/.oh-my-zsh/custom/themes/cadecolvin.zsh-theme
-echo "done"
+echo "done!"
 
 echo "Log out and back in for changes to take affect"
